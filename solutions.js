286c719b3077aeb69ee9658fa03508a0bea923a7
Evan Mead <evan.c.mead@gmail.com>
2020-10-07 15:10:58 -0500

    completed practice assignment - all correct

--- solutions.js
+++ solutions.js
@@ -21,29 +21,90 @@ function isOdd(input) {
 //TODO: Define a function named isVowel that takes in a single character and returns true if the character is a vowel. *Not including the letter 'y'
 
 function isVowel(char) {
-    return (char == "a" || "A" || "e" || "E" || "i" || "I" || "o" || "O" || "u" || "U");
+    // return (char === "a" || char === "A" || char === "e" || char === "E" || char === "i" || char === "I" || char === "o" || char === "O" || char === "u" || char === "U");
+
+    var vowels = "aeiouAEIOU";
+    return typeof (char) === "string" && char.length === 1 && vowels.includes(char);
 }
 
 //TODO: Define a function named combineStrings that takes in two parameters, if both parameters are strings, then return a concatenated string of both parameters. Otherwise, return false.
 
+function combineStrings(str1, str2) {
+    if (typeof str1 === "string" && typeof str2 === "string") {
+        return (str1 + str2);
+    } else {
+        return false
+    }
+}
 
 //TODO: Define a function named sumArgs that takes in three parameters, if they are numbers then add them together. Otherwise return false.
 
-
+function sumArgs(x, y, z) {
+    if (typeof x === "number" && typeof y === "number" && typeof z === "number") {
+        return (x + y + z);
+    } else {
+        return false;
+    }
+}
 
 //TODO: Define a function named truncateString that takes in a string as the first parameter and a number as the second parameter. Truncate (shorten) the string by the length based on the argument passed in. For example, truncateString("hello world",1) would return "hello worl"
 
+function truncateString(str, num) {
+    if (typeof str === "string" && typeof num === "number") {
+        return str.substring(0, str.length - num);
+    } else {
+        return false;
+    }
+}
 
 //TODO: Define a function named `reverseSign` that takes in a number as the first parameter. Reverse the sign of the argument passed in, return false if the argument is not a number. For example, `reverseSign(1)` would return `-1`.
 
+function reverseSign(num) {
+    if (num === "" || typeof num === "boolean" || typeof num === "object") {
+        return false;
+    } else if (!isNaN(num)) {
+        return num * -1;
+    } else {
+        return false;
+    }
+}
 
 //TODO: Define a function named `makeUppercase` that takes in a string as the first parameter. Return the argument passed in entirely uppercased, return false if the argument is not a string. For example, `makeUppercase("test")` would return `"TEST"`.
 
+function makeUppercase(str) {
+    if (typeof str === "string" && isNaN(str)) {
+        return str.toUpperCase();
+    } else {
+        return false;
+    }
+}
 
 //TODO: Define a function named `isMultiple` that takes in two `number` parameters. Return `true` if one argument is a multiple of the other, `false` otherwise. For example, `isMultiple(2, 6)` would return `true`.
 
+function isMultiple(num1, num2) {
+    if (num1 === num2) {
+        return false;
+    } else {
+        return num2 % num1 === 0;
+    }
+}
 
 //TODO: Define a function named `canPurchase` that takes in two `number` parameters, `billAmount` & `availableCash`. Return `true` if `availableCash` is greater than `billAmount`, `false` otherwise. For example, `canPurchase(10, 20)` would return `true`.
 
+function canPurchase(availableCash, billAmount) {
+    if (billAmount > availableCash) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 //TODO: Define a function named `getInitials` that takes in two `string` parameters, `first` & `last`. Return the names combined and formatted as initials, return `false` if either parameter is invalid. For example, `getInitials("John", "Snow")` would return `"J.S."`
+
+function getInitials(first, last) {
+    if (typeof first === "string" && typeof last === "string" && isNaN(first) && first !== "") {
+        return first.substring(0,1) + "." + last.substring(0,1) + ".";
+    } else {
+        return false;
+    }
+}
